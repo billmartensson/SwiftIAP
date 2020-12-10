e1df387c23ed32b453b6768e66b0cae12094fbec
Bill Mårtensson <bill@magictechnology.se>
2020-12-10 09:08:01 +0100

    Initial Commit

---
+++ SwiftIAP/SwiftIAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIAPApp.swift
+//  SwiftIAP
+//
+//  Created by Bill Martensson on 2020-12-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
